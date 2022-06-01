d277e0a7e678d9a623dde49373a76c93dd19bf73
Cedzol <90751132+Cedzol@users.noreply.github.com>
2022-06-01 15:52:34 +0200

    changed to atomic structure

--- aufgaben/src/App.tsx
+++ aufgaben/src/App.tsx
@@ -3,23 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import { AppBar, Box, Grid } from '@mui/material';
 import { Container } from '@mui/system';
+import './components/Molecules/RandomCatForm'
+import './components/Molecules/RandomCatList'
+import RandomCatList from "./components/Molecules/RandomCatList";
+import RandomCatForm from "./components/Molecules/RandomCatForm";
 
 function App() {
   return (
     <div className="App">
       <Box>
         <Container>
-          <AppBar>
+          <AppBar></AppBar>
 
-          </AppBar>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <p>Form here</p>
-            </Grid>
-            <Grid item xs={12}>
-              <p>List here</p>
-            </Grid>
-          </Grid>
+          <RandomCatForm></RandomCatForm>
+          <RandomCatList></RandomCatList>
+
         </Container>
       </Box>
     </div>
