548bbbf822a0bad2ac6583e1372fd13bdb95caf0
Cedzol <90751132+Cedzol@users.noreply.github.com>
2022-06-01 14:14:45 +0200

    project initial setup

--- aufgaben/src/App.tsx
+++ aufgaben/src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { AppBar, Box, Grid } from '@mui/material';
+import { Container } from '@mui/system';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box>
+        <Container>
+          <AppBar>
+
+          </AppBar>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <p>Form here</p>
+            </Grid>
+            <Grid item xs={12}>
+              <p>List here</p>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
     </div>
   );
 }
